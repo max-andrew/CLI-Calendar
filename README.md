2966fb54d721c03812843d09eb69dbb9ffd628c4
maxwell-andrew <andrewmr@bc.edu>
2017-08-13 21:33:16 -0400

    Add README, terminal demo screenshot, and adjust calendar.c
    Calendar.c: adjust label wording, and spacing

---
+++ README.md
@@ -0,0 +1,8 @@
+# Command Line Interface Calendar
+### C demonstration
+
+Build a calendar in terminal given a month's length and starting day.
+
+### Initialization
+
+Compile C code, then run in terminal. Follow subsequent prompts.
--- calendar.c
+++ calendar.c
@@ -1,5 +1,3 @@
-// count.c  Count the number of lines and chars in the input.
-
 #include <stdio.h>
 
 int main() {
@@ -12,41 +10,41 @@ int main() {
   // column counter - what col is being printed for current row (1->20)
   int col = 1;
 
-  /*GET USER INPUT*/
-  printf("Enter number of days month: ");
-  scanf("%d",&term);
+  /* GET USER INPUT */
+  printf("Enter number of days in month: ");
+  scanf("%d", &term);
   printf("Enter starting day of the week (1=Sun, 7=Sat): ");
-  scanf("%d",&first);
+  scanf("%d", &first);
   
-  /*GET TO FIRST DAY*/
+  /* GET TO FIRST DAY */
   // if month starts on 1st
   if (first == curr) {
-	//% width d
-	printf("%3d", curr);
-    curr++;
-	col++;
+  	// % width d
+  	printf("%3d", curr);
+      curr++;
+  	col++;
   }
   // any other day
   else {
-	while (col < first) {
-		printf("   ");
-		col++;
-	}
+  	while (col < first) {
+  		printf("   ");
+  		col++;
+  	}
   }
   
-  /*FINISH CALENDAR*/
+  /* FINISH CALENDAR */
   // walk through loop printing day
   while (curr <= term) {
-	// end of row
-	if (col%8 == 0) {
-		col = 1;
-		printf("\n");
-	}
-	// print day
-	printf("%3d", curr);
-	// update current day
-	curr++;
-	col++;
+  	// end of row
+  	if (col%8 == 0) {
+  		col = 1;
+  		printf("\n");
+  	}
+  	// print day
+  	printf("%3d", curr);
+  	// update current day
+  	curr++;
+  	col++;
   }
   printf("\n");
 }
